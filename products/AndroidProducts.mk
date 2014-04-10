1e855d976c533d695c36b4ec3541520ae29148d1
Zach <zachf714@gmail.com>
2014-04-10 11:09:11 -0400

    vendor: inc bringup
    Change-Id: I7577fbeaf415afbe38e20009cb8e54ed39d25787

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -17,6 +17,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/i9100.mk \
     $(LOCAL_DIR)/i9100g.mk \
     $(LOCAL_DIR)/i9300.mk \
+    $(LOCAL_DIR)/inc.mk \
     $(LOCAL_DIR)/jflte.mk \
     $(LOCAL_DIR)/l900.mk \
     $(LOCAL_DIR)/ls980.mk \
---
+++ products/inc.mk
@@ -0,0 +1,18 @@
+# Inherit device configs for inc
+$(call inherit-product, device/htc/inc/full_inc.mk)
+
+# Inherit GSM common stuff
+$(call inherit-product, vendor/aokp/configs/cdma.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/aokp/configs/common.mk)
+
+# Setup device specific product configuration.
+PRODUCT_NAME := aokp_inc
+PRODUCT_BRAND := HTC
+PRODUCT_DEVICE := inc
+PRODUCT_MODEL := Droid Incredible
+PRODUCT_MANUFACTURER := HTC
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=inc BUILD_FINGERPRINT="verizon_wwe/inc/inc/inc:2.3.4/GRJ22/185897.2:user/release-keys" 
+PRIVATE_BUILD_DESC="inc-user 4.4.2 KVT49L e9b6b7e1a7 release-keys"
